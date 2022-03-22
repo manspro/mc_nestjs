ebd7efc3b1fefbb2812ebd6e04dfca6839ff0dd5
Mansur Makhmutov <sir.makhmutov@gmail.com>
2022-03-22 21:17:40 +0300

    tag entity

---
+++ src/tag/tag.service.ts
@@ -0,0 +1,16 @@
+import { TagEntity } from './tag.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TagService {
+  //tagRepository это переменная-wraper для работы с таблицей TagEntity
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
+  }
+}
